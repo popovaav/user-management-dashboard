f12f228016d68be13778b76dd76192b5fee511dd
popovaav <82604296+popovaav@users.noreply.github.com>
2023-10-26 23:09:24 +0300

    fix search on server side

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,14 +1,24 @@
-const graphql = require('graphql');
-const axios = require('axios');
-const { GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList, GraphQLNonNull} = require('graphql');
+const graphql = require("graphql");
+const axios = require("axios");
+const {
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+} = require("graphql");
 
-const { GraphQLObjectType, } = graphql;
+const { GraphQLObjectType } = graphql;
+
+let allUsers = [];
+axios.get(`http://localhost:3001/users`).then((response) => {
+  allUsers = response.data;
+});
 
 function sortUsers(users, sortField, sortOrder) {
   return users.sort((a, b) => {
-    if (sortOrder === 'asc') {
+    if (sortOrder === "asc") {
       return a[sortField] > b[sortField] ? 1 : -1;
-    } else if (sortOrder === 'desc') {
+    } else if (sortOrder === "desc") {
       return a[sortField] < b[sortField] ? 1 : -1;
     } else {
       return a[sortField] > b[sortField] ? 1 : -1;
@@ -16,24 +26,41 @@ function sortUsers(users, sortField, sortOrder) {
   });
 }
 
+function filterUsers(users, searchName, searchAge) {
+  let filteredUsers = [...users];
+
+  if (searchName) {
+    const searchNameLower = searchName.toLowerCase();
+    filteredUsers = filteredUsers.filter((user) =>
+      user.firstName.toLowerCase().includes(searchNameLower),
+    );
+  }
+
+  if (searchAge) {
+    filteredUsers = filteredUsers.filter((user) => user.age === +searchAge);
+  }
+
+  return filteredUsers;
+}
+
 const TotalItemsType = new GraphQLObjectType({
-  name: 'TotalItems',
+  name: "TotalItems",
   fields: () => ({
     total: { type: GraphQLInt },
-  })
+  }),
 });
 
 const UserType = new GraphQLObjectType({
-  name: 'User',
+  name: "User",
   fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
-  })
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
+  name: "RootQueryType",
   fields: {
     users: {
       type: new GraphQLList(UserType),
@@ -42,31 +69,49 @@ const RootQuery = new GraphQLObjectType({
         page: { type: GraphQLInt },
         sortField: { type: GraphQLString },
         sortOrder: { type: GraphQLString },
+        searchName: { type: GraphQLString },
+        searchAge: { type: GraphQLString },
       },
       resolve(parentValue, args) {
-        const { first, page, sortField, sortOrder } = args;
+        const { first, sortField, sortOrder, searchName, searchAge, page } =
+          args;
+
+        let users = allUsers;
+
+        users = filterUsers(users, searchName, searchAge);
+
+        users = sortUsers(users, sortField, sortOrder);
+
         const offset = (page - 1) * first;
-        return axios.get(`http://localhost:3001/users`)
-          .then(response => {
-            let users = response.data;
-            if (sortField && sortOrder) {
-              users = sortUsers(users, sortField, sortOrder);
-            }
-            return users.slice(offset, offset + first);
-          });
-      }
+        const endIndex = offset + first;
+
+        if (users.length <= first) {
+          return users;
+        }
+
+        if (endIndex > users.length) {
+          return users.slice(first);
+        }
+
+        return users.slice(offset, endIndex);
+      },
     },
     totalItems: {
       type: TotalItemsType,
+      args: {
+        searchName: { type: GraphQLString },
+        searchAge: { type: GraphQLString },
+      },
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3001/users`)
-          .then(response => {
-            return { total: response.data.length };
-          });
-      }
-    }
+        const { searchName, searchAge } = args;
+
+        filterUsers(allUsers, searchName, searchAge);
+
+        return { total: filterUsers(allUsers, searchName, searchAge).length };
+      },
+    },
   },
-})
+});
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
